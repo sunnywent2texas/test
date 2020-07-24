42c18410097598b2e12bc0734985578e9058f99c
sunnywent2texas <41925742+sunnywent2texas@users.noreply.github.com>
2020-07-24 13:29:10 +0200

    test

--- test.py
+++ test.py
@@ -1,29 +1,14 @@
 from __future__ import absolute_import
-
 import os
 import sys
-
 from my_lib import Object, Object2, Object3
-from third_party import (
-    lib1,
-    lib2,
-    lib3,
-    lib4,
-    lib5,
-    lib6,
-    lib7,
-    lib8,
-    lib9,
-    lib10,
-    lib11,
-    lib12,
-    lib13,
-    lib14,
-    lib15,
-)
+from my_lib import Object, Object2, Object3
+from my_lib import Object, Object2, Object3
 
 test = 3534
 
 
-def test():
-    print("hello")
+
+
+def test( ) :
+      print( "hello" )
