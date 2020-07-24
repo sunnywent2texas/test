5acc72ff37078c765fefbb1d46fb03af3efb232a
sunnywent2texas <sunnywent2texas@users.noreply.github.com>
2020-07-24 12:45:25 +0000

    FIX: Auto-Format Python code

--- test.py
+++ test.py
@@ -1,14 +1,12 @@
 from __future__ import absolute_import
+
 import os
 import sys
-from my_lib import Object, Object2, Object3
-from my_lib import Object, Object2, Object3
+
 from my_lib import Object, Object2, Object3
 
 test = 3534
 
 
-
-
-def test( ) :
-      print( "hello" )
+def test():
+    print("hello")
